700e2dff7afaa0aa4425c2f8a33590179662dc8c
siive <idxntgiveafvck@hotmail.com>
2026-08-15 12:57:53 -0600

    Update catalog-data.js

--- catalog-data.js
+++ catalog-data.js
@@ -1,1 +1,26 @@
+/*
+ * CATALOGO INDEPENDIENTE
+ * ----------------------
+ * Este archivo será nuestra nueva fuente de datos.
+ *
+ * NO BORRAR:
+ * Por ahora app.js sigue utilizando catalog.js.
+ * Este archivo funcionará en paralelo mientras hacemos la migración.
+ */
 
+window.CATALOGO_CONFIG = {
+
+    version: "1.0.0",
+
+    planes: {},
+
+    precios: {},
+
+    vigencias: {},
+
+    preciosFuturos: {},
+
+    variantes: {},
+
+    equipos: {}
+};
